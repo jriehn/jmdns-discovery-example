60055424103e41137bde715649d7df36feb033d5
Jan Riehn <jan.riehn@web.de>
2014-10-14 14:46:45 +0200

    @jriehn: first shot

---
+++ src/test/java/SimplePrintListener.java
@@ -0,0 +1,27 @@
+/**
+ * Created by jriehn on 14/10/2014.
+ */
+
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceListener;
+
+/**
+ * Created by jriehn on 14/10/2014.
+ */
+public class SimplePrintListener implements ServiceListener {
+
+    @Override
+    public void serviceAdded(ServiceEvent event) {
+        System.out.println("Service added   : " + event.getName() + "." + event.getType()+":"+event.getInfo().getPort());
+    }
+
+    @Override
+    public void serviceRemoved(ServiceEvent event) {
+        System.out.println("Service removed : " + event.getName() + "." + event.getType());
+    }
+
+    @Override
+    public void serviceResolved(ServiceEvent event) {
+        System.out.println("Service resolved: " + event.getInfo());
+    }
+}
